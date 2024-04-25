1eed1c13656db1df1d5bf189054d969ff6b46257
Anupam Mondal <a09051985@gmail.com>
2024-04-25 23:06:04 +0530

    250420242306

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/index";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
